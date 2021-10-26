d41870a7326f77f6c5743dd565acfb3e8e1b4363
liaohaorui <liaohaorui@gamegoing.net>
2021-10-26 10:38:28 +0800

    动画模块：渐变进度条组件代码补充

--- layer_base/anim/src/main/java/com/example/anim/GameSeekBar.kt
+++ layer_base/anim/src/main/java/com/example/anim/GameSeekBar.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
-
 /**
  * @author lhr
  * @date 2021/10/24
@@ -20,6 +19,8 @@ class GameSeekBar : View{
 
     private val fgPaint: Paint = Paint()
 
+    private val indexPaint: Paint = Paint()
+
     private var maxSchedule = 5
 
     private var curSchedule = 4
@@ -32,11 +33,16 @@ class GameSeekBar : View{
         bgPaint.isAntiAlias = true
         bgPaint.strokeCap = Paint.Cap.ROUND
         bgPaint.strokeWidth = paintWidth
-        bgPaint.color = Color.GRAY
+        bgPaint.color = Color.parseColor("#EBEBEB")
 
         fgPaint.isAntiAlias = true
         fgPaint.strokeCap = Paint.Cap.ROUND
         fgPaint.strokeWidth = paintWidth
+
+        indexPaint.isAntiAlias = true
+        indexPaint.style = Paint.Style.FILL
+        indexPaint.strokeWidth = paintWidth
+        indexPaint.color = Color.parseColor("#FF6414")
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -56,28 +62,43 @@ class GameSeekBar : View{
     }
 
     private fun drawBackground(canvas: Canvas){
-        val circleDistance = width / (maxSchedule - 1f)
+        val circleDistance = (width - paintWidth * 2f) / (maxSchedule - 1f)
+        val startX = paintWidth / 1f
+        val startY = height / 3f * 2f
 
-        canvas.drawLine(0f,height/2f,circleDistance * (maxSchedule - 1f),height/2f,bgPaint)
+        canvas.drawLine(startX, startY,circleDistance * (maxSchedule - 1f), startY, bgPaint)
         for (i in 0 until maxSchedule){
-            canvas.drawCircle(circleDistance * i, height/2f, paintWidth, bgPaint)
+            canvas.drawCircle(startX + circleDistance * i, startY, paintWidth, bgPaint)
         }
     }
 
     private fun drawForeground(canvas: Canvas){
-        val circleDistance = width / (maxSchedule - 1f)
+        val circleDistance = (width - paintWidth * 2f) / (maxSchedule - 1f)
+        val startX = paintWidth / 1f
+        val startY = height / 3f * 2f
         val linearGradient = LinearGradient(
-            0f,height/2f,
-            circleDistance * (curSchedule - 1),height/2f,
-            Color.parseColor("#ff0000"),Color.parseColor("#00ff00"),
+            startX,startY,
+            circleDistance * (curSchedule - 1),startY,
+            Color.parseColor("#FEA235"),Color.parseColor("#FF6414"),
             Shader.TileMode.CLAMP
         )
         fgPaint.shader = linearGradient
-        canvas.drawLine(0f,height/2f,circleDistance * (curSchedule - 1),height/2f,fgPaint)
+        canvas.drawLine(startX,startY,circleDistance * (curSchedule - 1),startY,fgPaint)
 
         for (i in 0 until curSchedule){
-            canvas.drawCircle(circleDistance * i, height/2f, paintWidth, fgPaint)
+            canvas.drawCircle(startX + circleDistance * i, startY, paintWidth, fgPaint)
         }
+
+        //绘制浮标
+        val lastIndexX = startX + circleDistance * (curSchedule - 1)
+        val path = Path()
+        path.fillType = Path.FillType.EVEN_ODD
+        path.moveTo(lastIndexX - paintWidth/2, 0f)
+        path.lineTo(lastIndexX - paintWidth/2, 0f)
+        path.lineTo(lastIndexX + paintWidth/2, 0f)
+        path.lineTo(lastIndexX, 15f)
+        path.close()
+        canvas.drawPath(path,indexPaint)
     }
 
 }
